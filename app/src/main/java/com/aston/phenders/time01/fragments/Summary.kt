f05092a12aa01d98793b029d2e71586fb6a9b91c
pete1551 <peterhenderson95@hotmail.co.uk>
2018-08-27 22:25:51 +0100

    set correct month on load

--- app/src/main/java/com/aston/phenders/time01/fragments/Summary.kt
+++ app/src/main/java/com/aston/phenders/time01/fragments/Summary.kt
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import com.aston.phenders.time01.R
@@ -14,6 +15,9 @@ import com.aston.phenders.time01.adapters.TimeCardRecyclerAdapter
 import com.aston.phenders.time01.database.DatabaseHelper
 import com.aston.phenders.time01.database.TimeTable
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.warn
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class Summary : Fragment(), AnkoLogger {
@@ -28,6 +32,10 @@ class Summary : Fragment(), AnkoLogger {
         super.onCreate(savedInstanceState)
         var view = inflater.inflate(R.layout.fragment_summary, container, false)
 
+        val dateNow: Calendar = Calendar.getInstance()
+
+        var test: ArrayList<Int> = ArrayList()
+
         summaryRecycler = view.findViewById(R.id.summary_recycler)
         summaryRecycler!!.layoutManager = LinearLayoutManager(context)
         summaryRecycler!!.adapter = timeCardAdapter
@@ -38,15 +46,27 @@ class Summary : Fragment(), AnkoLogger {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         monthSpinner!!.adapter = adapter
 
+
+
         yearSpinner = view.findViewById(R.id.year_spinner) as Spinner
         val yearSpinnerAdapter = ArrayAdapter.createFromResource(context,
                 R.array.years_array, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
         yearSpinner!!.adapter = yearSpinnerAdapter
 
+        monthSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
 
-        monthSpinner!!.setSelection(7)//change
-        yearSpinner!!.setSelection(0)//change
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                getTimeCards()
+            }
+
+        }
+
+
+        monthSpinner!!.setSelection(dateNow.get(Calendar.MONTH))
+        yearSpinner!!.setSelection(0)//change to get current year
 
 
         return view
@@ -54,7 +74,19 @@ class Summary : Fragment(), AnkoLogger {
 
     override fun onResume() {
         super.onResume()
+    }
 
+    fun getTodaysMonthAsInt() {
+
+        TODO("Not Implemented")
+    }
+
+    fun getTodaysYearAsInt() {
+
+        TODO("Not Implemented")
+    }
+
+    private fun getTimeCards() {
         var db = DatabaseHelper(activity!!.applicationContext)
 
         var month: String = monthSpinner!!.selectedItem.toString()
@@ -67,16 +99,5 @@ class Summary : Fragment(), AnkoLogger {
 
         timeCardAdapter.currentResults.addAll(timeBookings)
         timeCardAdapter.notifyDataSetChanged()
-
-    }
-
-    fun getTodaysMonthAsInt() {
-
-        TODO("Not Implemented")
-    }
-
-    fun getTodaysYearAsInt() {
-
-        TODO("Not Implemented")
     }
 }
