3b05c2b9b874ce1b00f4ccec7fc6dc9738f9c746
pete1551 <peterhenderson95@hotmail.co.uk>
2018-09-13 13:11:54 +0100

    switched to server

--- app/src/main/java/com/aston/phenders/time01/api/Urls.kt
+++ app/src/main/java/com/aston/phenders/time01/api/Urls.kt
@@ -1,10 +1,14 @@
 package com.aston.phenders.time01.api
 
 object Urls {
+
+    /* DEBUG LOCAL SERVER
     val getUrl = "http://10.0.2.2:81/Time/API/GetTime.php"
     val putUrl = "http://10.0.2.2:81/Time/API/PutTime.php"
     val deleteUrl = "http://10.0.2.2:81/Time/API/DeleteTime.php"
-    //val getUrl = "http://51.141.126.64/Time/API/GetTime.php"
-    //val putUrl = "http://51.141.126.64/Time/API/PutTime.php"
-    //val deleteUrl = "http://51.141.126.64/Time/API/DeleteTime.php"
+    */
+    /*Azure Server*/
+    val getUrl = "http://51.141.126.64/Time/API/GetTime.php"
+    val putUrl = "http://51.141.126.64/Time/API/PutTime.php"
+    val deleteUrl = "http://51.141.126.64/Time/API/DeleteTime.php"
 }
